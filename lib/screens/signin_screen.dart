ad27ef6dd5082ab3b1e86c216b4d86bf135c630f
alielhadidy99 <alielhadidy99@gmail.com>
2020-09-21 12:42:53 +0200

    init

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,153 @@
+import 'package:chatapp/helper/helperfunctions.dart';
+import 'package:chatapp/screens/chatrooms.dart';
+
+import 'package:chatapp/services/auth.dart';
+import 'package:chatapp/services/database.dart';
+import 'package:chatapp/widgets/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  final Function toggleView;
+  SignInScreen(this.toggleView);
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController emailEditingController = new TextEditingController();
+  TextEditingController passwordEditingController = new TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  AuthService authService = new AuthService();
+  signIn() async {
+    if (formKey.currentState.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+
+      await authService
+          .signInWithEmailAndPassword(emailEditingController.text.trim(),
+              passwordEditingController.text.trim())
+          .then((result) async {
+        if (result != null) {
+          QuerySnapshot userInfoSnapshot =
+              await DatabaseMethods().getUserInfo(emailEditingController.text);
+
+          HelperFunctions.saveUserLoggedInSharedPreference(true);
+          HelperFunctions.saveUserNameSharedPreference(
+              userInfoSnapshot.documents[0].data["userName"]);
+          HelperFunctions.saveUserEmailSharedPreference(
+              userInfoSnapshot.documents[0].data["userEmail"]);
+
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => ChatRoom()));
+        } else {
+          setState(() {
+            isLoading = false;
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: isLoading
+          ? Container(
+              child: Center(child: CircularProgressIndicator()),
+            )
+          : Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: ListView(
+                children: [
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val)
+                                ? null
+                                : "Please Enter Correct Email";
+                          },
+                          controller: emailEditingController,
+                          style: simpleTextStyle(),
+                          decoration: textFieldInputDecoration("email"),
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          validator: (val) {
+                            return val.length > 6
+                                ? null
+                                : "Enter Password 6+ characters";
+                          },
+                          style: simpleTextStyle(),
+                          controller: passwordEditingController,
+                          decoration: textFieldInputDecoration("password"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      signIn();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          gradient: LinearGradient(
+                            colors: [
+                              const Color(0xff007EF4),
+                              const Color(0xff2A75BC)
+                            ],
+                          )),
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        "Sign In",
+                        style: biggerTextStyle(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have account? ",
+                        style: simpleTextStyle(),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          widget.toggleView();
+                        },
+                        child: Text(
+                          "Register now",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,164 @@
+import 'package:chatapp/helper/helperfunctions.dart';
+
+import 'package:chatapp/screens/chatrooms.dart';
+import 'package:chatapp/services/auth.dart';
+import 'package:chatapp/services/database.dart';
+import 'package:chatapp/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  final Function toggleView;
+  SignUpScreen(this.toggleView);
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUpScreen> {
+  TextEditingController emailEditingController = new TextEditingController();
+  TextEditingController passwordEditingController = new TextEditingController();
+  TextEditingController usernameEditingController = new TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  AuthService authService = new AuthService();
+  DatabaseMethods databaseMethods = new DatabaseMethods();
+
+  singUp() async {
+    if (formKey.currentState.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+
+      await authService
+          .signUpWithEmailAndPassword(emailEditingController.text.trim(),
+              passwordEditingController.text.trim())
+          .then((result) {
+        if (result != null) {
+          Map<String, String> userDataMap = {
+            "userName": usernameEditingController.text.trim(),
+            "userEmail": emailEditingController.text.trim()
+          };
+          databaseMethods.addUserInfo(userDataMap);
+          HelperFunctions.saveUserLoggedInSharedPreference(true);
+          HelperFunctions.saveUserEmailSharedPreference(
+              emailEditingController.text.trim());
+          HelperFunctions.saveUserNameSharedPreference(
+              usernameEditingController.text.trim());
+
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => ChatRoom()));
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: isLoading
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: ListView(
+                children: [
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          style: simpleTextStyle(),
+                          controller: usernameEditingController,
+                          validator: (val) {
+                            return val.isEmpty || val.length < 3
+                                ? "Enter Username 3+ characters"
+                                : null;
+                          },
+                          decoration: textFieldInputDecoration("username"),
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: emailEditingController,
+                          style: simpleTextStyle(),
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val)
+                                ? null
+                                : "Enter correct email";
+                          },
+                          decoration: textFieldInputDecoration("email"),
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          style: simpleTextStyle(),
+                          decoration: textFieldInputDecoration("password"),
+                          controller: passwordEditingController,
+                          validator: (val) {
+                            return val.length < 6
+                                ? "Enter Password 6+ characters"
+                                : null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      singUp();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          gradient: LinearGradient(
+                            colors: [
+                              const Color(0xff007EF4),
+                              const Color(0xff2A75BC)
+                            ],
+                          )),
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        "Sign Up",
+                        style: biggerTextStyle(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account? ",
+                        style: simpleTextStyle(),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          widget.toggleView();
+                        },
+                        child: Text(
+                          "SignIn now",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
